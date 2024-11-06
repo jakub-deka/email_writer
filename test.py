ef5be819779cc5bf10f72a46dc52ec0f1418883a
Jakub Deka <120565768+jakub-deka@users.noreply.github.com>
2024-11-06 10:26:34 +0000

    checking in

--- test.py
+++ test.py
@@ -8,8 +8,7 @@ prompt = t.generate(
     "prompt_refiner.md",
     prompt="We are 118118Money. We offer loans from £1k to £5k from 12 months to 5 years with representative APR of 49.9%. Write an engaging marketing email to encourage the customers to log in to their account to get a chance to enter into a prize draw.",
 )
-print(prompt)
 
-m.generate(prompt)
-
+prompt2 = m.generate(prompt, keep_history=False)
+m.generate(prompt2)
 m.print_last_message()
